824bf1edcc5584128fac0dd032534e2267c453d9
John Clarke <john.clarke@cornerstonenw.com>
2014-06-09 16:28:43 -0400

    Extracted user params to userconfig.py

---
+++ script/userconfig.py
@@ -0,0 +1,4 @@
+global config
+config = {'conn_string':'', 'output_path':''}
+config['conn_string'] = "host='gis' port='5432' dbname='postgres' user='postgres' password=''"
+config['output_path'] = 'C:/Users/John/Documents/GitHub/vivo/script/'
