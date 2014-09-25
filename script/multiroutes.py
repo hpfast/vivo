96269ff272e38839eb17bfd42d4cc979fd24ebbf
Hans Fast <hans@surfacemarkup.net>
2014-09-25 19:45:18 +0200

    finished multiroutes version 1

--- script/multiroutes.py
+++ script/multiroutes.py
@@ -50,24 +50,19 @@ def main():
     #The main datastructure is an array representing all the rountes.
     # Each route is a dict with key = hospID and value empty ready to hold geometries.
     routes = [] 
-    #hosps = {}
+    inputlines = []
 #    print sys.argv[1]
     try:
         arg = sys.argv[1]
         input_file = open(arg, 'r')
         inputline = input_file.readline()
         while inputline:
-        #for inputline in input_file.readline():
-#            print inputline
             line = inputline.rstrip()
-            #route[i] = l #we'll need this later outside this scope
             ls = line.split(' ')
             ls = [int(i) for i in ls]
             hospar = ls
-            hosps = dict((l,{}) for l in ls) #convert list to dict
-            routes.append(hosps) #put the dict of hospitals into the array of routes
+            inputlines.append(hospar) #put the array of hospital ids into the array of routes
             inputline = input_file.readline()
-#        print routes
         input_file.close()
     except:
         exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
@@ -91,64 +86,103 @@ def main():
         sys.exit("error connecting to database\n ->%s" % (exceptionValue))
 
 
-    # -----------------------------------------------------
-    # FETCH DATA FROM DATABASE AND MERGE INTO ROUTES OBJECT
-    # -----------------------------------------------------
-
-    for route in routes:
-        db_matches = db_fetch(cursor, route)
-        for f in db_matches:
-            for h in route:
+    # -------------------------------------------------
+    # FETCH DATA FROM DATABASE AND MERGE WITH HOSPITALS
+    # -------------------------------------------------
+    print inputlines
+    lines2=[]
+    for hosps in inputlines:
+        line = {}
+        ids = []
+        points = []
+        db_matches = db_fetch(cursor, hosps)
+        for h in hosps:
+            for f in db_matches:
                 if f[1] == h:
-                    route[h].update({'name':f[0],'geom':f[2]}) #geom is still well-known binary format here
+                    ids.append(h)
+                    points.append(loads(a2b_hex(f[2])))
+        line.update({'ids':ids,'points':points})
+        xys = LineString([l.x,l.y] for l in line['points'])
+        line.update({'geom':xys})
+        line.update({'start_id':line['ids'][0],'end_id':line['ids'][-1]})
+        lines2.append(line)
 
-
+    # ------------            
+    # GEOJSONIFY
+    # ------------
+    
+    #list to hold features
+    col = []
+    
+    #create a geojson Feature out of every item in lines
+    i = 0;
+    for r in lines2:
+        feature = geojson.Feature(
+            geometry=lines2[i]['geom'],
+            id=i+1,
+            properties = {k: v for k, v in lines2[i].iteritems() if k != 'geom'} #everything not geom goes in props
+        ) 
+        col.append(feature)
+        i+=1
+    
+    #make all features into a featurecollection
+    collection = geojson.FeatureCollection(col)
+    
+    #order is in order_id key.
+ 
+    #write featurecollection to file
+    print os.path.normpath(config['output_path'])+'lines.geojson'
+    with open(os.path.normpath(config['output_path']) + '/lines.geojson', 'w') as outfile:
+        outfile.write(geojson.dumps(collection))
+    outfile.close()
+    print "wrote features to "+os.path.normpath(config['output_path'])+"lines.geojson"
     # -------------------------------------
-    # DRAW LINES BETWEEN CONSECUTIVE POINTS
+    # DRAW LINES BETWEEN POINTS
     # -------------------------------------
-    lines = []
     #convert geometries in hosps to shapely objects (from well-known-binary format received from postgis)
-    for route in routes:
-        for h in route:
-            route[h]['geom'] = loads(a2b_hex(route[h]['geom'])) #postgis uses hex encoding, need to account for this
+#    for route in routes:
+#        line = []
+#        for h in route:
+#            route[h]['geom'] = loads(a2b_hex(route[h]['geom'])) #postgis uses hex encoding, need to account for this
+#            line.append(dict((str(first)+"_"+str(second),{}) for first,second in grouper(list(route), 2)))
     #TODO: modify the following to create an array of key, multilinestring. In two steps? first create a multilinestring for each route, then
     #line can be as below
     #multiline the joined lines for each route
     #lines the container to hold them all.
         #create a new dict to hold our line features
         #initially contains key and empty dict as value
-        #lines = dict((str(first)+"_"+str(second),{}) for first,second in grouper(list(route), 2))
         
         #add geometry and properties to lines dict
         #loop over every consecutive pair from input points
         #we can thus add attrs of both points, and create a line between them.
-       # i = 1
-       # for first, second in grouper(hospar, 2):
-       #     for l in lines:
-       #         if l == str(first)+"_"+str(second):
-       #             #get x and y coords from shapely point objects   
-       #             x1 = hosps[first]['geom'].x
-       #             y1 = hosps[first]['geom'].y
-       #             x2 = hosps[second]['geom'].x
-       #             y2 = hosps[second]['geom'].y
-       #             #create an item in lines with id and name of start/end hosps, and linestring geometry
-       #             lines[l].update({
-       #                 'start_id':first,
-       #                 'start_name': hosps[first]['name'],
-       #                 'end_id':second,
-       #                 'end_name':hosps[second]['name'],
-       #                 'order_id': i,
-       #                 #here comes the new linestring geometry
-       #                 'geom':LineString([(x1,y1),(x2,y2)]),
-       #                 'line_id':l
-       #                 #uses shapely LineString constructor with x,y coords of start and end points
+#        i = 1
+        #for first, second in grouper(list(route), 2):
+           # print "first is "+str(first) +", second is "+str(second)
+        #    for l in line:
+               # if l == str(first)+"_"+str(second):
+#                    print "hello!!!!!!!!!!!!!"
+                    #get x and y coords from shapely point objects   
+               #     x1 = route[first]['geom'].x
+               #     y1 = route[first]['geom'].y
+               #     x2 = route[second]['geom'].x
+               #     y2 = route[second]['geom'].y
+                    #create an item in lines with id and name of start/end hosps, and linestring geometry
+               #     line[l].update({
+               #         'start_id':first,
+               #         'start_name': hosps[first]['name'],
+               #         'end_id':second,
+               #         'end_name':hosps[second]['name'],
+               #         'order_id': i,
+               #         #here comes the new linestring geometry
+               #         'geom':LineString([(x1,y1),(x2,y2)]),
+               #         'line_id':l
+                        #uses shapely LineString constructor with x,y coords of start and end points
 
-       #                 # JBC: thought the following might work to add errors, but it couldn't serialize the output 
-       #                 # 'geom':pylab.arrow(x1, y1, x2, y2, color='#999999', aa=True, head_width=1.0, head_length=1.0),
-       #             })
-       #     i+=1;       
+                        # JBC: thought the following might work to add errors, but it couldn't serialize the output 
+                        # 'geom':pylab.arrow(x1, y1, x2, y2, color='#999999', aa=True, head_width=1.0, head_length=1.0),
+               #     })
+           # i+=1;       
 
-    print routes
     #end main
 
 #db fetch function. A separate query for each route -- sufficient performance for small route sets.
