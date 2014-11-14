50e22a740396cf231f08fd99753b70e950b05d33
Hans Fast <hans@surfacemarkup.net>
2014-11-14 13:47:19 +0100

    testing out routing of data flow through app

--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,67 @@
 var app = app || {};
 
-app.base_url = 'http://128.199.53.137:8080';
+//app.base_url = 'http://128.199.53.137:8080';
+app.base_url = 'http://192.168.122.116:8080';
 app.url = '/routes/byrun/latest';
 
+
+/**
+ * Fast UUID generator, RFC4122 version 4 compliant.
+ * @author Jeff Ward (jcward.com).
+ * @license MIT license
+ * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+ **/
+
+
+app.UUID = (function() {
+  var self = {};
+  var lut = []; for (var i=0; i<256; i++) { lut[i] = (i<16?'0':'')+(i).toString(16); }
+  self.generate = function() {
+    var d0 = Math.random()*0xffffffff|0;
+    var d1 = Math.random()*0xffffffff|0;
+    var d2 = Math.random()*0xffffffff|0;
+    var d3 = Math.random()*0xffffffff|0;
+    return lut[d0&0xff]+lut[d0>>8&0xff]+lut[d0>>16&0xff]+lut[d0>>24&0xff]+'-'+
+      lut[d1&0xff]+lut[d1>>8&0xff]+'-'+lut[d1>>16&0x0f|0x40]+lut[d1>>24&0xff]+'-'+
+      lut[d2&0x3f|0x80]+lut[d2>>8&0xff]+'-'+lut[d2>>16&0xff]+lut[d2>>24&0xff]+
+      lut[d3&0xff]+lut[d3>>8&0xff]+lut[d3>>16&0xff]+lut[d3>>24&0xff];
+  }
+  return self;
+})();
+
+app.testdata = {string:'very cool stuff!'};
+//This creates a new container of maps given an API url.
+app.Container = function(event,data) {
+    //all our model and view will be grouped by container.
+    //so generate a UUID for this container.
+    data.uuid = app.UUID.generate();
+    app.filterRoutes(null,data);
+    this.tmpl = $.templates('#mainTmpl');
+    this.html = this.tmpl.render(app.testdata);
+    this.ourcontainer = $('<div id="'+data.uuid+'">');
+    this.ourcontainer.promise().done(function(){$(app).trigger('done')});
+    this.ourcontainer.html(this.html);
+    console.log('catch');
+    $('#testdiv').append(this.ourcontainer);
+    
+    this.destroy = function(member){
+        delete this.member;
+    }
+        
+    
+}
+
+app.createContainer = function(event, data) {
+    event.target.data = data;
+    app.newcontainer = new app.Container(event, data);   
+}
+
+////from af
+//    var tmpl = $.templates('#hrchyTmpl');
+//    var html = tmpl.render(app.mydata, {foo:'hello',add: app.sumThings,sumtotal: app.sumTotal, format:app.formatNum,max:app.max,isNumeric:app.isNumeric,oneN:app.oneN,countDecimals:app.countDecimals});//lots of helper functions passed along.oneN: app.oneN
+//    $("#content-container").html(html)
+//        .promise().done(function(){$(app).trigger('done')
+//    }); //register done event
 
 //constructor to create a pane with Leaflet map and proper data bindings
 app.Map = function (num, sol_id) { //temporary solution: distinguish with solution id.
@@ -42,19 +101,14 @@ app.solutionBuilder = function(event, data){
 app.mapBuilder = function (event, data) {
     var routes_by_month = app.groupBy(data, function(item){return item.month});
     var month_count = Object.keys(routes_by_month).length;
-    for (var i = 0; i<month_count;i++){ //replace this with a count of months. We'll always be making maps by month.
+    for (var i = 0; i<month_count;i++){ 
         app.maps.push(new app.Map(i, routes_by_month[i][0].sol_id));
-        for (var o = 0; o<routes_by_month[i].length;o++){ //replace this with a count of months. We'll always be making maps by month.
+        for (var o = 0; o<routes_by_month[i].length;o++){ 
             routes_by_month[i][o].route = routes_by_month[i][o].route.replace(/{/, '').replace(/}/, '').split(',');
             $(app).trigger('mapready', {route_id: routes_by_month[i][o].route_id,sol_id: routes_by_month[i][o].sol_id});
         }
 
     }
-//    for (var i = 0; i<data.length;i++){ //replace this with a count of months. We'll always be making maps by month.
-//        data[i].route = data[i].route.replace(/{/, '').replace(/}/, '').split(',');
-//        app.maps.push(new app.Map(i, data[i].sol_id));
-//        $(app).trigger('mapready', {route_id: data[i].route_id,map_id: i});
-//    }
     $(app).trigger('mapsadded');
     
 }
@@ -108,6 +162,7 @@ app.addGeodata = function(){
 
 //get the route, look up the points by hosp id and make a line and add it to the map
 app.drawRoutes = function(event, data){
+    console.log(event);
     console.log(data.sol_id);
     var results = app.getObjects(app.data,'route_id',data.route_id, 'route');
     var route = results[0]; //just one for now
@@ -135,6 +190,7 @@ app.filterRoutes = function(event, data){
     var data_filtered;
     if (data.r == 'byrun'){
         data_filtered = app.groupBySolution(null, data);
+        app.group(null,data);
     }else{
         console.log('only testing byrun for now');
     }
@@ -144,7 +200,7 @@ app.filterRoutes = function(event, data){
 
 //learned this on http://codereview.stackexchange.com/questions/37028/grouping-elements-in-array-by-multiple-properties
 //modified to create an array of named objects rather than of arrays
-app.groupBy = function ( array , f ) {
+app.groupBy = function ( array , f, type) {
       var groups = {};
       array.forEach( function( o )
       {
@@ -152,12 +208,15 @@ app.groupBy = function ( array , f ) {
         groups[group] = groups[group] || [];
         groups[group].push( o );  
       });
+    if (type && type == 'object'){
         return groups;
+    } else if (type && type == 'array'){
         //enable below instead to return arrays
-    //  return Object.keys(groups).map( function( group )
-    //  {
-    //    return groups[group]; 
-    //  })
+      return Object.keys(groups).map( function( group )
+      {
+        return groups[group]; 
+      })
+    }
 
 }
 
@@ -173,6 +232,24 @@ app.groupBySolution = function(event, data){
     
 }
 
+//group by month, solution, and run.
+app.group = function(event, data){
+    var data_grouped = {};
+    var temp_sol = {};
+    var by_run = app.groupBy(data.d, function(item){return item.run_id}, 'object');
+    for (var i in by_run){
+        temp_sol[i] = app.groupBy(by_run[i], function(item){return item.sol_id}, 'object');
+        data_grouped[i] = {};
+        for (var j in temp_sol[i]){
+            data_grouped[i][j] = app.groupBy(temp_sol[i][j],function(item){return item.month}, 'object');
+        }
+    };
+    console.log(data_grouped);    
+        
+        
+        
+}
+
 //TODO: we need to group by month, count how many months there are, and draw that many maps,
 //keeping track of map ids to put
 app.groupByMonth = function(event, data){
@@ -181,23 +258,21 @@ app.groupByMonth = function(event, data){
 }
 
 app.dataFetcher = function(url){
-    var base_url = app.base_url;
-    var custom_url = url || app.url;
     $.ajax({
         //type: 'POST',
         type: 'GET',
         //url: 'data/seed.json',
        //url:'http://128.199.53.137:8080/routes/byid/1',
         //TODO: get url from location bar or user input on page
-        url: base_url+custom_url,
+        url: url,
         // url: 'http://192.168.122.116:3000/db/postgres/schemas/public/tables/',
         //data: request,
         dataType: 'json'
     }).done(function(data,statusText,jqXHR){
         app.data=data;
         
-        //pass on data and request endpoint
-        $(app).trigger('datafetched',{d:data,r:this.url.match('/routes/(.*)/')[1]});
+        //pass on data and request endpoint type
+        $(app.testdata).trigger('datafetched',{d:data,r:this.url.match('/routes/(.*)/')[1]});
         
     }).fail(function(data,statusText,jqXHR){
         console.log(statusText,jqXHR);
@@ -207,7 +282,7 @@ app.dataFetcher = function(url){
     
 }
 
-
+//encapsulate these as a module which can be run
 app.selectDetector = function (event, e) {
     e.preventDefault();
     var t;
@@ -247,11 +322,11 @@ app.prepareRoutesRequest = function(event,data){
         event.preventDefault();
         endpoint = event.target[0].value;
         route = $(event.target[0]).attr('data-type');
-        app.dataFetcher(composeUrl(route)+endpoint);
+        app.dataFetcher(app.base_url+composeUrl(route)+endpoint);
     
     //select 'latest'
     } else if (event.type='request-routes') {
-        app.dataFetcher(composeUrl(data));
+        app.dataFetcher(app.base_url+composeUrl(data));
     }
     return false;
 }
@@ -260,14 +335,16 @@ $(document).ready(function ($) {
     
     //$(app).on('geodataparsed',app.addGeodata); //demo function: to test if the geojson works
     
+    $(app).on('done',function(event,data){alert('this event fired!')});
     //TODO: before calling mapbuilder, run the grouping routines to end up with routes grouped by month.
-    $(app).on('datafetched',app.filterRoutes); //produces 'routesfiltered'
+    $(app.testdata).on('datafetched',app.createContainer);
+    //$(app).on('datafetched',app.filterRoutes); //produces 'routesfiltered'
     $(app).on('routesfiltered',app.solutionBuilder);
     //$(app).on('datafetched',app.mapBuilder);
     $(app).on('geodatafetched',function(event,data){
         app.geodata.hospitals = JSON.parse(data);
         $(app).trigger('geodataparsed');
-        app.dataFetcher();
+        app.dataFetcher(app.base_url+app.url);
     });
     $(app).on('mapready', app.drawRoutes);
     
